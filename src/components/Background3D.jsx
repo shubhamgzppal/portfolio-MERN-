7ce570ec250efe58738c3b523f5dae78ace9bfe8
shubhamgzppal <shubhamgzppal@gmail.com>
2025-07-26 20:50:27 +0530

    hiii

--- src/components/Background3D.jsx
+++ src/components/Background3D.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 // components/Background3D.jsx
-const Background3D = ({ theme = 'dark', darkVideo = "/mixkit-smoke-effect-over-black-background-1967-full-hd.mp4", lightVideo = "/100195-video-2160.mp4" }) => {
+const Background3D = ({ theme = 'dark', darkVideo = "/src/assets/mixkit-smoke-effect-over-black-background-1967-full-hd.mp4", lightVideo = "/src/assets/100195-video-2160.mp4" }) => {
   const isLight = theme === 'light';
   return (
     <div className="absolute inset-0 -z-10 w-full h-full">
