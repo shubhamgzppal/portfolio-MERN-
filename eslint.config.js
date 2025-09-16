fae7276d075104fd3b7c4f2430414098bbdf5310
Shubham pal <Shubhamgzppal@gmail.com>
2025-09-16 18:18:45 +0530

    chore: update ESLint configuration and add Netlify configuration file

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,3 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-
-export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      env: { browser: true, es2021: true },
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: { jsx: true },
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
 module.exports = {
   root: true,
   env: {
@@ -38,15 +5,25 @@ module.exports = {
     node: true,
     es2021: true,
   },
-  extends: ['next', 'next/core-web-vitals', 'eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
   parserOptions: {
     ecmaFeatures: { jsx: true },
     ecmaVersion: 2021,
     sourceType: 'module',
   },
-  settings: { react: { version: 'detect' } },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
     'react/react-in-jsx-scope': 'off',
     'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
   },
+  ignorePatterns: ['dist'],
 };
